c0c6e324b539d6981e996510cc42d579c2c0bed5
kpadmabala@gmail.com <kpadmabala@gmail.com>
2020-01-02 12:30:36 +0530

    working on the app

---
+++ src/app/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from "../store/index";
+import {ConnectedDashboard} from "../components/Dashboard"
+export const Main=()=>(
+    <Provider store={store}>
+        <div>
+            {/* Dashboard goes here */}
+            <ConnectedDashboard/>
+        </div>
+    </Provider>
+)
--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
-console.log("Hello world");
-
 import {store} from './store';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Main} from "./components/Main";
 
-console.log(store.getState());
+ReactDOM.render(
+    <Main/>,
+    document.getElementById("app")
+)
+
